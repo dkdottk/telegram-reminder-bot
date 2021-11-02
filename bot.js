dbcfb523850a3c4c99ee91d84994e6616047e8b8
Danil <dkdottk@gmail.com>
2021-11-02 15:35:54 +0200

    Initial commit

---
+++ bot.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const { Telegraf, Markup, Scenes, session, Stage} = require('telegraf');
+
+const token = process.env.BOT_TOKEN
+if (token === undefined) {
+    throw new Error('BOT_TOKEN must be provided!')
+}
+
+const bot = new Telegraf(token, {
+    telegram: { webhookReply: true }
+})
+
+const stage = new Scenes.Stage([
+
+]);
+
+bot.start((ctx) => ctx.reply('Hello'))
+
+bot.use(session())
+bot.use(stage.middleware());
+
+bot.launch()
+
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
